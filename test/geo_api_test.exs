6326e2e0485e67c6e417e80832bebb5ac4b49b5e
Getty Orawo <gettyorawo@gmail.com>
2021-06-05 06:15:48 +0300

    add test case

--- test/geo_api_test.exs
+++ test/geo_api_test.exs
@@ -2,7 +2,7 @@ defmodule GeoApiTest do
   use ExUnit.Case
   doctest GeoApi
 
-  test "greets the world" do
-    assert GeoApi.hello() == :world
+  test "saves valid records or rejects invalid ones" do
+    assert GeoWorld.import_csv("data_dump_test.csv") == %{accepted: 1, rejected: 1, time_elapsed_in_microsecs: 243203, total_records: 2}
   end
 end
