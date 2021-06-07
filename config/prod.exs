1a4649ab0e6ac5f7755ab3ca67483322458b1775
Getty Orawo <gettyorawo@gmail.com>
2021-06-07 15:32:06 +0300

    remove ssl

--- config/prod.exs
+++ config/prod.exs
@@ -2,5 +2,4 @@ use Mix.Config
 
 config :geo_api, GeoApi.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "geo-worlds.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "geo-worlds.herokuapp.com/", port: 80]
