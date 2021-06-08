ed95b0176c7d221acc92d790530fc840f149a8ae
Getty Orawo <gettyorawo@gmail.com>
2021-06-08 22:01:53 +0300

    configure database for local only

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 # Docker entrypoint script.
 
 # Wait until Postgres is ready
-while ! pg_isready -q -h $PGHOST -p $PGPORT -U $PGUSER
+while ! pg_isready -q --host=db --port=5432 --username=postgres
 do
   echo "$(date) - waiting for database to start"
   sleep 2
@@ -10,7 +10,7 @@ done
 
 # Create, migrate, and seed database if it doesn't exist.
 if [[ -z `psql -Atqc "\\list postgres"` ]]; then
-  echo "Database $PGDATABASE does not exist. Creating..."
+  echo "Database postgres does not exist. Creating..."
   mix ecto.create
   mix ecto.migrate
   echo "Database $PGDATABASE created."
